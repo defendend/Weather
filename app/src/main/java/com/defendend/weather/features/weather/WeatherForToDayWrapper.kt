c12827e3e83e68e8b1904f0303a0eaaf8c9f4cd4
Alexandr <79938520+defendend@users.noreply.github.com>
2022-04-06 23:03:29 +0300

    add WeatherForToDayWrapper.kt for json response from weather api server

--- app/src/main/java/com/defendend/weather/features/weather/WeatherForToDayWrapper.kt
+++ app/src/main/java/com/defendend/weather/features/weather/WeatherForToDayWrapper.kt
@@ -1,116 +1,215 @@
 package com.defendend.weather.features.weather
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
 data class WeatherForToDayWrapper(
+    @SerialName("alerts")
     val alerts: List<Alert>,
+    @SerialName("current")
     val current: Current,
+    @SerialName("daily")
     val daily: List<Daily>,
+    @SerialName("hourly")
     val hourly: List<Hourly>,
+    @SerialName("lat")
     val lat: Double,
+    @SerialName("lon")
     val lon: Double,
+    @SerialName("timezone")
     val timezone: String,
-    val timezone_offset: Int
+    @SerialName("timezone_offset")
+    val timezoneOffset: Int
 )
 
+@Serializable
 data class Alert(
+    @SerialName("description")
     val description: String,
+    @SerialName("end")
     val end: Int,
+    @SerialName("event")
     val event: String,
-    val sender_name: String,
+    @SerialName("sender_name")
+    val senderName: String,
+    @SerialName("start")
     val start: Int,
+    @SerialName("tags")
     val tags: List<String>
 )
 
+@Serializable
 data class Current(
+    @SerialName("clouds")
     val clouds: Int,
-    val dew_point: Double,
+    @SerialName("dew_point")
+    val dewPoint: Double,
+    @SerialName("dt")
     val dt: Int,
-    val feels_like: Double,
+    @SerialName("feels_like")
+    val feelsLike: Double,
+    @SerialName("humidity")
     val humidity: Int,
+    @SerialName("pressure")
     val pressure: Int,
+    @SerialName("snow")
     val snow: Snow,
+    @SerialName("sunrise")
     val sunrise: Int,
+    @SerialName("sunset")
     val sunset: Int,
+    @SerialName("temp")
     val temp: Double,
+    @SerialName("uvi")
     val uvi: Int,
+    @SerialName("visibility")
     val visibility: Int,
+    @SerialName("weather")
     val weather: List<Weather>,
-    val wind_deg: Int,
-    val wind_gust: Double,
-    val wind_speed: Double
+    @SerialName("wind_deg")
+    val windDeg: Int,
+    @SerialName("wind_gust")
+    val windGust: Double,
+    @SerialName("wind_speed")
+    val windSpeed: Double
 )
 
+@Serializable
 data class Daily(
+    @SerialName("clouds")
     val clouds: Int,
-    val dew_point: Double,
+    @SerialName("dew_point")
+    val dewPoint: Double,
+    @SerialName("dt")
     val dt: Int,
-    val feels_like: FeelsLike,
+    @SerialName("feels_like")
+    val feelsLike: FeelsLike,
+    @SerialName("humidity")
     val humidity: Int,
-    val moon_phase: Double,
+    @SerialName("moon_phase")
+    val moonPhase: Double,
+    @SerialName("moonrise")
     val moonrise: Int,
+    @SerialName("moonset")
     val moonset: Int,
+    @SerialName("pop")
     val pop: Int,
+    @SerialName("pressure")
     val pressure: Int,
+    @SerialName("rain")
     val rain: Double,
+    @SerialName("snow")
     val snow: Double,
+    @SerialName("sunrise")
     val sunrise: Int,
+    @SerialName("sunset")
     val sunset: Int,
+    @SerialName("temp")
     val temp: Temp,
+    @SerialName("uvi")
     val uvi: Double,
+    @SerialName("weather")
     val weather: List<WeatherX>,
-    val wind_deg: Int,
-    val wind_gust: Double,
-    val wind_speed: Double
+    @SerialName("wind_deg")
+    val windDeg: Int,
+    @SerialName("wind_gust")
+    val windGust: Double,
+    @SerialName("wind_speed")
+    val windSpeed: Double
 )
 
+@Serializable
 data class FeelsLike(
+    @SerialName("day")
     val day: Double,
+    @SerialName("eve")
     val eve: Double,
+    @SerialName("morn")
     val morn: Double,
+    @SerialName("night")
     val night: Double
 )
 
+@Serializable
 data class Hourly(
+    @SerialName("clouds")
     val clouds: Int,
-    val dew_point: Double,
+    @SerialName("dew_point")
+    val dewPoint: Double,
+    @SerialName("dt")
     val dt: Int,
-    val feels_like: Double,
+    @SerialName("feels_like")
+    val feelsLike: Double,
+    @SerialName("humidity")
     val humidity: Int,
+    @SerialName("pop")
     val pop: Double,
+    @SerialName("pressure")
     val pressure: Int,
+    @SerialName("rain")
     val rain: Rain,
+    @SerialName("snow")
     val snow: SnowX,
+    @SerialName("temp")
     val temp: Double,
+    @SerialName("uvi")
     val uvi: Int,
+    @SerialName("visibility")
     val visibility: Int,
+    @SerialName("weather")
     val weather: List<WeatherXX>,
-    val wind_deg: Int,
-    val wind_gust: Double,
-    val wind_speed: Double
+    @SerialName("wind_deg")
+    val windDeg: Int,
+    @SerialName("wind_gust")
+    val windGust: Double,
+    @SerialName("wind_speed")
+    val windSpeed: Double
 )
 
+@Serializable
 data class SnowX(
-    val `1h`: Double
+    @SerialName("1h")
+    val oneHour: Double
 )
 
+@Serializable
 data class Temp(
+    @SerialName("day")
     val day: Double,
+    @SerialName("eve")
     val eve: Double,
+    @SerialName("max")
     val max: Double,
+    @SerialName("min")
     val min: Double,
+    @SerialName("morn")
     val morn: Double,
+    @SerialName("night")
     val night: Double
 )
 
+@Serializable
 data class WeatherX(
+    @SerialName("description")
     val description: String,
+    @SerialName("icon")
     val icon: String,
+    @SerialName("id")
     val id: Int,
+    @SerialName("main")
     val main: String
 )
 
+@Serializable
 data class WeatherXX(
+    @SerialName("description")
     val description: String,
+    @SerialName("icon")
     val icon: String,
+    @SerialName("id")
     val id: Int,
+    @SerialName("main")
     val main: String
 )
