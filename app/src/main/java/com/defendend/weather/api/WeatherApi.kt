ea5f49aa27497719280fc19f077c63d74fe14d1d
Alexandr <79938520+defendend@users.noreply.github.com>
2022-04-06 23:29:00 +0300

    GET to weather from cityName

--- app/src/main/java/com/defendend/weather/api/WeatherApi.kt
+++ app/src/main/java/com/defendend/weather/api/WeatherApi.kt
@@ -10,17 +10,18 @@ interface WeatherApi {
     suspend fun getWeatherFromGeolocation(
         @Query("lat") latitude: String,
         @Query("lon") longitude: String,
+        @Query("units") units: String = "metric",
         @Query("lang") language: String = "ru",
         @Query("appid") API_KEY: String
     ): WeatherWrapper
 
-//    @GET("/")
-//    suspend fun getWeatherFromCityName(
-//        @Query("q") cityName: String,
-//        @Query("units") units: String = "metric",
-//        @Query("lang") language: String = "ru",
-//        @Query("appid") API_KEY: String
-//    ): String
+    @GET("data/2.5/weather")
+    suspend fun getWeatherFromCityName(
+        @Query("q") cityName: String,
+        @Query("units") units: String = "metric",
+        @Query("lang") language: String = "ru",
+        @Query("appid") API_KEY: String
+    ): WeatherWrapper
     object Constants {
         const val API_KEY = "65026bbc1e33bb883cd79a2a0f8955b9"
         const val API_URL = "https://api.openweathermap.org/"
